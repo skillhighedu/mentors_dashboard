026ab103246427e7545ec5d858c9c75fa40691bd
Sai Kiran <neelakantamsaikiran2@gmail.com>
2025-02-17 13:37:05 +0530

    file structer

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+<div className='text-2xl text-blue-800 '>Mentor</div>
+    </>
+  )
+}
+
+export default App
